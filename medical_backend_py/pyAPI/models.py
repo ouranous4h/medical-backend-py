ef3056eb62d65a16fffeec0a459ff906b438375d
Kanat Bektursyn <kanatbektursyn@Kanats-MacBook-Air.local>
2021-03-07 01:37:18 +0500

    login register

--- medical_backend_py/pyAPI/models.py
+++ medical_backend_py/pyAPI/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class userP(models.Model):
+  username = models.CharField(max_length=30, null = False, blank = False, unique = False)
+  email = models.CharField(max_length=30, null = False, blank = False, unique = False)
+  password = models.CharField(max_length=30, null = False, blank = False, unique = False)
+
 ## hospital -> doctors -> patients
 ## patients -> medical card, doctors
 ## maybe make it inherited from human? ex: patient -> human, doctor -> human (name, surname, mname, age, phone, etc)
 class Patient(models.Model):
   ## add foreign keys -> doctors
   # -> medical card
+  # -> foreign key userP id
   name = models.CharField(max_length=30, null = False, blank = False, unique = False)
   surname = models.CharField(max_length=30, null = False, blank = False, unique = False)
   middlename = models.CharField(max_length=30, null = True, blank = True, unique = False)
